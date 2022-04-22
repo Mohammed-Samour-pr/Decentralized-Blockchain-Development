9da246e7e017296c67ab0c6e924c6b608c2c1327
Mohammed <mohammed.samour.pr@gmail.com>
2022-04-22 23:12:47 +0300

    Mohammed

---
+++ src/min/BlockChainTest.java
@@ -0,0 +1,25 @@
+package min;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class BlockChainTest extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        BorderPane root = FXMLLoader.load(ClassLoader.getSystemResource("main/resources/fxml/BlockChainTest.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("BlockChain");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        Application.launch(BlockChainTest.class, args);
+    }
+
+}
